9e18d3752fdf63280292a3fc7a9b627e670239f0
Carlos Fletes <carlos.fletes@marvelsa.com>
2019-08-30 15:51:17 -0500

    1.0.1

--- models/sugerido.py
+++ models/sugerido.py
@@ -7,3 +7,9 @@ class SurtidoSugerido (models.Model):
     _inherit = 'res.partner'
 
     sugerido = fields.Char(string='Surtido Sugerido',)
+
+
+class QoutationSurtidoSugerido(models.Model):
+    _inherit = 'sale.order'
+    qou_sugerido = fields.Char(related='partner_id.sugerido',
+                               string="Surtido Sugerido", readonly=True)
